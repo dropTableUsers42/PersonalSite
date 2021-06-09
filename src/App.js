356b68f81e831886505338774e0a2506e94e2619
dropTableUsers42 <rwitaban0001@gmail.com>
2021-06-10 02:26:57 +0530

    Use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/pages/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as HashRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Footer from './components/pages/Footer';
 
 function App(){
 	return (
-		<Router>
+		<HashRouter basename='/'>
 			<Navbar />
 			<div className="page">
 				<Switch>
-					<Route path='/'>
+					<Route exact path='/'>
 						<Home />
 					</Route>
 				</Switch>
 			</div>
 			<Footer />
-		</Router>
+		</HashRouter>
 	);
 }
 
